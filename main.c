beef77bdfdce8b493e93baf28292ec1614d23581
João Pedro Melchiori <137243559+jpmelchiori@users.noreply.github.com>
2025-10-28 00:16:20 -0300

    INICIANDO O MAIN.C

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
 int main(){
+    int opcao;
+    double base , altura , raio , comprimento , volume , densidade;
+    
+    do{
+        printf("========================");
+        printf("CALCULADORA ENGENHARIA");
+        printf("========================");
 
-}
+
+    }while
+
+    }
